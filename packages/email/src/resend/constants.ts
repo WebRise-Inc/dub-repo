7617aec71075d6217e17bbab27df919ace9b900a
Rabih Daoud <71355928+rabi3daoudd@users.noreply.github.com>
2025-06-17 23:57:09 -0400

    working

--- packages/email/src/resend/constants.ts
+++ packages/email/src/resend/constants.ts
@@ -4,7 +4,7 @@ export const RESEND_AUDIENCES = {
 };
 
 export const VARIANT_TO_FROM_MAP = {
-  primary: "Dub.co <system@dub.co>",
-  notifications: "Dub.co <notifications@mail.dub.co>",
-  marketing: "Steven from Dub.co <steven@ship.dub.co>",
+  primary: "WebRise <otp@notifications.webrise.ca>",
+  notifications: "WebRise <notifications@notifications.webrise.ca>",
+  marketing: "WebRise <marketing@notifications.webrise.ca>",
 };
